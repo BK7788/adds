87213504a048ee4fb6e9fa08220b841415abd824
BK7788 <a1938000@adelaide.edu.au>
2025-08-09 23:42:13 +0930

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,9 +3,16 @@
 #include "Human.h"
 #include "Computer.h"
 int main() {
- std::cout << "Monkey-Robot-Pirate-Ninja-Zombie + Rock-Paper-Scissors\n";
- std::cout << "Valid moves: Rock, Paper, Scissors, Robot, Monkey, Pirate, Ninja, Zomb
- ie\n\n";
- Human h1("Human");
- 
+    std::cout << "Monkey-Robot-Pirate-Ninja-Zombie + Rock-Paper-Scissors\n";
+    std::cout << "Valid moves: Rock, Paper, Scissors, Robot, Monkey, Pirate, Ninja, Zomb ie\n\n";
+    Human h1("Human");
+    Computer cpu("CPU");
+    Referee ref; 13:
+    Player* winner = ref.refGame(&h1, &cpu);
+    if (winner) {
+        std::cout << "Winner: " << winner->getName() << "\n";
+    } else {
+        std::cout << "No winner this round.\n";
+    }
+    return 0;
 }

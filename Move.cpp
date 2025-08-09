d9e62f8ab1c5e3f53ff8a1110d32fcaff8839095
BK7788 <a1938000@adelaide.edu.au>
2025-08-09 23:56:40 +0930

    Create Move.cpp

---
+++ Move.cpp
@@ -0,0 +1,15 @@
+#ifndef MOVE_H
+#define MOVE_H
+#include <string>
+#include <memory>
+
+class Move {
+public:
+    virtual ~Move() = default; 12:
+    virtual std::string getName() const = 0;
+    virtual bool defeats(const Move& other) const = 0;
+};
+Move* createMoveByName(const std::string& fullName);
+
+std::string toLowerCopy(const std::string& s);
+#endif

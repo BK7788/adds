d005a8d0abd6ca83696ba9a7cb93ea5950df2100
BK7788 <a1938000@adelaide.edu.au>
2025-08-09 23:47:46 +0930

    Update Referee.h

--- Referee.h
+++ Referee.h
@@ -1,9 +1,11 @@
 #ifndef REFEREE_H
 #define REFEREE_H
 #include "Player.h"
+
 class Referee {
 public:
-    Referee();
+    Referee() = default;
+    // Returns pointer to winning Player; nullptr if draw/invalid (undefined in spec).
     Player* refGame(Player* player1, Player* player2);
-};
-#endif
+}; 12:
+#endif // REFEREE_H

c5724759a5ffe6ebfdab533b5514557726110c0a
BK7788 <a1938000@adelaide.edu.au>
2025-08-09 23:37:28 +0930

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,11 @@
 #include <iostream>
+#include "Referee.h"
 #include "Human.h"
 #include "Computer.h"
-#include "Referee.h"
-using namespace std;
 int main() {
- // You can change the human name here if needed
-    Human human("Mei");
-    Computer computer;
-    Referee referee;
-    Player* winner = referee.refGame(&human, &computer);
-    if (winner == nullptr) {
-        cout << "It's a Tie" << endl;
-    } else {
-        cout << winner->getName() << " Wins" << endl;
-    }
-    return 0;
+ std::cout << "Monkey-Robot-Pirate-Ninja-Zombie + Rock-Paper-Scissors\n";
+ std::cout << "Valid moves: Rock, Paper, Scissors, Robot, Monkey, Pirate, Ninja, Zomb
+ ie\n\n";
+ Human h1("Human");
+ 
 }

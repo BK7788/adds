956ff5d10337b57ded681dc767451f024a1973b7
BK7788 <a1938000@adelaide.edu.au>
2025-08-10 14:01:01 +0930

    Update Move.cpp

--- Move.cpp
+++ Move.cpp
@@ -4,9 +4,6 @@
 #include <unordered_map>
 #include <cctype>
 
-// ------------------------
-// Utilities
-// ------------------------
 std::string toLowerCopy(const std::string& s) {
     std::string out = s;
     std::transform(out.begin(), out.end(), out.begin(),

c24cc3577595941563b3e34322dc7889661732e9
BK7788 <a1938000@adelaide.edu.au>
2025-08-31 22:50:28 +0800

    Add Practical 4: Finder.cpp, Finder.h, main.cpp

--- Finder.cpp
+++ Finder.cpp
@@ -1,24 +1,13 @@
 #include "Finder.h"
 
-
-namespace {
-struct FastIO {
-    FastIO() {
-        std::ios::sync_with_stdio(false);
-        std::cin.tie(nullptr);
-    }
-} fastio;
-}
-
-
 static std::vector<int> prefix_function(const std::string& s){
     int n = (int)s.size();
-    std::vector<int> pi(n,0);
-    for (int i=1;i<n;i++){
-        int j = pi[i-1];
-        while (j>0 && s[i]!=s[j]) j = pi[j-1];
-        if (s[i]==s[j]) ++j;
-        pi[i]=j;
+    std::vector<int> pi(n, 0);
+    for (int i = 1; i < n; ++i) {
+        int j = pi[i - 1];
+        while (j > 0 && s[i] != s[j]) j = pi[j - 1];
+        if (s[i] == s[j]) ++j;
+        pi[i] = j;
     }
     return pi;
 }
@@ -28,21 +17,22 @@ std::vector<int> Finder::findSubstrings(const std::string& s1, const std::string
     const int n = (int)s1.size();
     const int m = (int)s2.size();
     std::vector<int> ans(m, -1);
-    if (m==0 || n==0) return ans;
+    if (m == 0 || n == 0) return ans;
 
     std::vector<int> pi = prefix_function(s2);
     int j = 0;
 
     for (int i = 0; i < n; ++i) {
-        while (j > 0 && s1[i] != s2[j]) j = pi[j-1];
+        while (j > 0 && s1[i] != s2[j]) j = pi[j - 1];
         if (s1[i] == s2[j]) ++j;
 
+        
         int t = j;
-        while (t > 0 && ans[t-1] == -1) {
-            ans[t-1] = i - t + 1;
-            t = pi[t-1];
+        while (t > 0 && ans[t - 1] == -1) {
+            ans[t - 1] = i - t + 1;
+            t = pi[t - 1];
         }
-        if (j == m) j = pi[j-1];
+        if (j == m) j = pi[j - 1];
     }
     return ans;
 }
--- Finder.h
+++ Finder.h
@@ -2,9 +2,21 @@
 #define FINDER_H
 
 #include <iostream>   
-#include <chrono>     
+#include <chrono>    
 #include <string>
 #include <vector>
+
+
+namespace finder_internal {
+struct FastIO {
+    FastIO() {
+        std::ios::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+    }
+};
+
+inline FastIO fastio_guard{};
+} 
 
 class Finder {
 public:
